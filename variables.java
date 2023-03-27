717b7f0035dd222d91c306c3b2a702a3816f22bb
srilakshmi-kalisetty <125849928+srilakshmi-kalisetty@users.noreply.github.com>
2023-03-27 07:10:37 -0800

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String args[])
+    {
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 7;
+        int num4 = 7;
+        System.out.println(num1+num2+num3+num4);
+    }
+    
+}
